ce9b904349be741de254f7c9c654869bd027fc5f
wathon315 <wathonnadisan2021@gmail.com>
2025-04-02 14:58:20 +0400

    application

---
+++ product.js
@@ -0,0 +1,48 @@
+let products = [
+    {
+        "id": 1001,
+        "title": "Cat Food, 25lb Bag",
+        "description": "A 25 pound bag of <em>irresistible</em>," + " organic goodness for your cat.",
+        "price": 2000,
+        "image": "images/food.jpeg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1002,
+        "title": "Cat Food, 25lb Bag",
+        "description": "A 25 pound bag of <em>irresistible</em>," + " organic goodness for your cat.",
+        "price": 2000,
+        "image": "images/food.jpeg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1003,
+        "title": "Cat Food, 25lb Bag",
+        "description": "A 25 pound bag of <em>irresistible</em>," + " organic goodness for your cat.",
+        "price": 2000,
+        "image": "images/food.jpeg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1004,
+        "title": "Cat Food, 25lb Bag",
+        "description": "A 25 pound bag of <em>irresistible</em>," + " organic goodness for your cat.",
+        "price": 2000,
+        "image": "images/food.jpeg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+    {
+        "id": 1005,
+        "title": "Cat Food, 25lb Bag",
+        "description": "A 25 pound bag of <em>irresistible</em>," + " organic goodness for your cat.",
+        "price": 2000,
+        "image": "images/food.jpeg",
+        "availableInventory": 10,
+        "rating": 2
+    },
+
+]
---
+++ server-worker.js
@@ -0,0 +1,46 @@
+var cacheName = 'petstore-v1';
+var cacheFiles = [
+    'index.html',
+    'products.js',
+    'pet.css',
+    'petstore.webmanifest',
+    'images/cat.jpg',
+    'images/food.jpg',
+    'images/icon-store-512.png'
+];
+
+self.addEventListener('install', (e) => {
+    console.log('[Service Worker] Install');
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            console.log('[Service Worker] Caching all the files');
+            return cache.addAll(cacheFiles);
+        })
+    );
+});
+
+// self.addEventListener('fetch', function (e) {
+//     e.respondWith(
+//         // check if the cache has the file
+//         caches.match(e.request).then(function (r) {
+//             console.log('[Service Worker] Fetching resource: ' + e.request.url);
+//             // 'r' is the matching file if it exists in the cache
+//             return r 
+//         })
+//     );
+// });
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith(
+        caches.match(e.request).then(function (r) {
+            // Download the file if it is not in the cache, 
+            return r || fetch(e.request).then(function (response) {
+                // add the new file to cache
+                return caches.open(cacheName).then(function (cache) {
+                    cache.put(e.request, response.clone());
+                    return response;
+                });
+            });
+        })
+    );
+});
